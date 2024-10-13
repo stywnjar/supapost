c0abe3bc2401c88e3ca16befa55e04f2533e8455
stywnjar <stywn.jar@gmail.com>
2024-10-13 15:31:16 -0400

    setup supabase

---
+++ src/libs/supabase/client.ts
@@ -0,0 +1,11 @@
+"use client";
+
+import { Database } from "@/types/supabase.types";
+import { createBrowserClient } from "@supabase/ssr";
+
+export function createClient() {
+  return createBrowserClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+}
