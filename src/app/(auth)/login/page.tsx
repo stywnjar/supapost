9eabe2b23e0480e0bb09e034c6d44da2144165e1
stywnjar <stywn.jar@gmail.com>
2024-10-13 15:36:39 -0400

    create auth pages

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Login",
+};
+
+export default function LoginPage() {
+  return (
+    <main>
+      <h1>Login page</h1>
+    </main>
+  );
+}
---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Register",
+};
+
+export default function RegisterPage() {
+  return (
+    <main>
+      <h1>Register page</h1>
+    </main>
+  );
+}
