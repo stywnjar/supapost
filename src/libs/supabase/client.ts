d3e91f76bd8f5913d7888dc609e2d814f3cbe229
stywnjar <stywn.jar@gmail.com>
2024-10-22 02:26:31 +0700

    up

--- src/libs/supabase/client.ts
+++ src/libs/supabase/client.ts
@@ -1,10 +1,9 @@
 "use client";
 
-import { Database } from "@/types/supabase.types";
 import { createBrowserClient } from "@supabase/ssr";
 
 export function createClient() {
-  return createBrowserClient<Database>(
+  return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
